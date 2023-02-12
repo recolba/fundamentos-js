a17eed98b4c616250f0ebfff05e1f6e661110541
recolba <renatocolbar@gmail.com>
2023-02-12 16:42:04 -0300

    exemplos da aula 1 - alura

---
+++ fundamentals.js
@@ -0,0 +1,59 @@
+// Tipo number
+
+const meuNumero = 3;
+
+const prim = 1;
+const seg = 2; 
+
+const operacaoMatematica = prim / seg;
+
+// console.log(operacaoMatematica);
+
+
+// ponto flutuante
+
+const flt1 =  3.3;
+const pontoFlutuante = .5;
+
+const resultado = flt1 + pontoFlutuante;
+
+// console.log(resultado);
+
+// Tipo string
+
+const texto = "senhassãosalvasviastringtbm";
+
+const oi = "exemplo vou dizer um 'oi!'";
+
+// concatenação
+
+const citacao = 'meu nome é ';
+const meuNome = "Renato"
+
+// console.log(citacao + meuNome)
+
+// template string ou template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+const dado = '\u1f618'
+
+// console.log(cifrao)
+// console.log(aMaiusculo)
+// console.log(tique)
+// console.log(hiragana)
+// console.log(dado)
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+// console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+// console.log(senha.length) // 13 caracteres
+
---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+// Boolean
+
+const primeiroNum = 5;
+const segundoNum = 5;
+let cadastroAtivado = false;
+
+console.log(primeiroNum === segundoNum);
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2);
+
