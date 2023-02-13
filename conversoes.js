3b5b07f02bff3d247b177a92acb514e08c497e97
recolba <renatocolbar@gmail.com>
2023-02-13 19:42:55 -0300

    códigos e conversões

---
+++ conversoes.js
@@ -0,0 +1,14 @@
+// tipo 
+// booleanos
+
+// conversão implícita
+const num = 456;
+const numString = Number("456");
+
+// Number()
+// String()
+console.log(num + numString)
+
+// 2 iguais é mesmo valor e não necessariamente o mesmo tipo de dado
+// 3 iguais é o mesmo valor e mesmo tipo de dado
+
---
+++ truthy-falsy.js
@@ -0,0 +1,25 @@
+// boolean
+const userlog = true;
+const contapaga = false;
+
+// truthy or falsy
+
+// false => 0 
+// true => 1 
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+// null and undefined
+
+let variavel;
+let vazio = null;
+
+// console.log(variavel)
+// console.log(vazio)
+
+let numero = 3;
+let texto = "Alura";
+console.log(typeof variavel)
+console.log(typeof vazio)
