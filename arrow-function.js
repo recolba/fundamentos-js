3b67eb83acd0296693791f012a1c1dea20aac655
recolba <renatocolbar@gmail.com>
2023-02-17 15:15:05 -0300

    updates

--- arrow-function.js
+++ arrow-function.js
@@ -26,3 +26,27 @@ console.log(somaNumPequenos(10, 9))
 
 // operador maior ou igual que
 // >= 
+
+
+const num = null;
+
+if (num === null) {
+ console.log('número não fornecido');
+}
+
+if (!num) {
+    console.log('número não fornecido');
+   }
+
+// const num = 15;
+// else if
+
+if (num >= 0 && num <= 10) {
+    console.log('número entre zero e dez');
+} else if (num > 10 && num <= 20) {
+    console.log('número entre dez e vinte');
+} else if (num > 20 && num <= 30) {
+console.log('número entre vinte e trinta');
+} else {
+    console.log('outro número');
+}
